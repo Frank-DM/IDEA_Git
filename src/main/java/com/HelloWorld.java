d35a96ba85a27a4e78c17c60478e6bbc806d1b78
Frank <liudaoming_better@163.com>
2019-06-02 19:16:30 +0800

    dev modified push to GitHub

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -3,5 +3,6 @@ package com;
 public class HelloWorld{
     public static void main(String [] args) {
         System.out.println("Add HelloWorld class");
+        System.out.println("modify on dev branch");
     }
 }
