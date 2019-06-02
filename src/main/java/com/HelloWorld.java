fc748247e936b5d26adaebd3e1f969df6b510322
Frank <liudaoming_better@163.com>
2019-06-02 19:04:32 +0800

    第一次修改，增加HelloWord类并且修改README.md文件

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -3,6 +3,5 @@ package com;
 public class HelloWorld{
     public static void main(String [] args) {
         System.out.println("Add HelloWorld class");
-        System.out.println("modify on dev branch");
     }
 }
