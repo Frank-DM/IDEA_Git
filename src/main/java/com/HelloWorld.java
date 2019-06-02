b76444e5c86b18a84fddbd6bebb10711d57798fe
Frank <liudaoming_better@163.com>
2019-06-02 19:00:19 +0800

    Share Project

---
+++ src/main/java/com/HelloWorld.java
@@ -0,0 +1,8 @@
+package com;
+
+public class HelloWorld{
+    public static void main(String [] args) {
+        System.out.println("Add HelloWorld class");
+        System.out.println("modify on dev branch");
+    }
+}
