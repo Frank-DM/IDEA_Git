8e81d8bc4f3f6ed408442d8e3c487952c46182a5
Frank <liudaoming_better@163.com>
2019-06-02 23:44:39 +0800

    版本回退后提交

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -4,7 +4,5 @@ public class HelloWorld{
     public static void main(String [] args) {
         System.out.println("Add HelloWorld class");
         System.out.println("modify on dev branch");
-
-        System.out.println("modify for test reset");
     }
 }
